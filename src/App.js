7ee7d5c0f518cd203954bdb77ecf9ef359dc6c94
Alexandru <alexandrundy@gmail.com>
2022-11-24 14:30:31 +0200

    adding navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
 import HomePage from './pages/homepage/homepage';
+import { Routes, Route } from 'react-router-dom';
+
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>HatsPage</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div>
-      <HomePage />
-    </div>
+    <Routes>
+      <Route exact path="/" element={<HomePage />} />
+      <Route exact path="/hats" element={<HatsPage />} />
+    </Routes>
   );
 }
 
