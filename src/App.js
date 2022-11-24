8ec8426daf640c254d800c2b808971ec95d9d87b
Alexandru <alexandrundy@gmail.com>
2022-11-24 15:25:21 +0200

    adding shop page,colection previw && item

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 import './App.css';
 import HomePage from './pages/homepage/homepage';
 import { Routes, Route } from 'react-router-dom';
-
-const HatsPage = () => {
-  return (
-    <div>
-      <h1>HatsPage</h1>
-    </div>
-  );
-};
+import ShopPage from './pages/shop/shopPage';
 
 function App() {
   return (
     <Routes>
       <Route exact path="/" element={<HomePage />} />
-      <Route exact path="/hats" element={<HatsPage />} />
+      <Route exact path="/shop" element={<ShopPage />} />
     </Routes>
   );
 }
