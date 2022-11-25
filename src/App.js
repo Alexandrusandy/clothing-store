3fc20c51aea7b2a2548161184d173bf2b35bf298
Alexandru <alexandrundy@gmail.com>
2022-11-25 12:55:39 +0200

    update sign in page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import HomePage from './pages/homepage/homepage';
 import { Routes, Route } from 'react-router-dom';
 import ShopPage from './pages/shop/shopPage';
 import Header from './componenets/header/header';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<HomePage />} />
         <Route exact path="/shop" element={<ShopPage />} />
+        <Route exact path="/signin" element={<SignInAndSignUpPage />} />
       </Routes>
     </div>
   );
