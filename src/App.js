d6e3100cabd27a92de3659d65bf4715b66ffa057
Alexandru <alexandrundy@gmail.com>
2022-11-25 10:24:09 +0200

    adding header componenet

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import './App.css';
 import HomePage from './pages/homepage/homepage';
 import { Routes, Route } from 'react-router-dom';
 import ShopPage from './pages/shop/shopPage';
+import Header from './componenets/header/header';
 
 function App() {
   return (
-    <Routes>
-      <Route exact path="/" element={<HomePage />} />
-      <Route exact path="/shop" element={<ShopPage />} />
-    </Routes>
+    <div>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route exact path="/shop" element={<ShopPage />} />
+      </Routes>
+    </div>
   );
 }
 
