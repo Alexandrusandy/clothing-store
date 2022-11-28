d59db37add7fd8797485fc95df28849c3a7b478c
Alexandru <alexandrundy@gmail.com>
2022-11-28 13:56:13 +0200

    fix cart bug

--- src/componenets/cart-icon/cart-icon.jsx
+++ src/componenets/cart-icon/cart-icon.jsx
@@ -1,22 +1,21 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
+
+import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
+
 import './cart-icon.styles.scss';
 
 const CartIcon = ({ toggleCartHidden }) => (
-  console.log('cart icon', toggleCartHidden),
-  (
-    <div className="cart-icon" onClick={toggleCartHidden}>
-      <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">0</span>
-    </div>
-  )
+  <div className="cart-icon" onClick={toggleCartHidden}>
+    <ShoppingIcon className="shopping-icon" />
+    <span className="item-count">0</span>
+  </div>
 );
 
 const mapDispatchToProps = (dispatch) => ({
-  toogleCartHidden: () => dispatch(toggleCartHidden),
+  toggleCartHidden: () => dispatch(toggleCartHidden()),
 });
 
 export default connect(null, mapDispatchToProps)(CartIcon);
