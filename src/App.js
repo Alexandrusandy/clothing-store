c533f02e52b866a1e9d7632c8d1d946de1a73057
Alexandru <alexandrundy@gmail.com>
2022-12-12 13:45:35 +0200

    updating shop componenet and app componenet to use hooks

--- src/App.js
+++ src/App.js
@@ -1,55 +1,39 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Header from './componenets/header/header';
 import { Routes, Route, Navigate } from 'react-router-dom';
 
-// import ShopPage from './pages/shop/shopPage';
 import HomePage from './pages/homepage/homepage';
+import ShopPage from './pages/shop/shopPage';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up';
 import CheckoutPage from './pages/checkout/checkout';
 
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { selectCurrentUser } from './redux/user/user.selector';
-import ShopPageWrapper from './pages/shop/ShopPageWrapper';
 import { checkUserSession } from './redux/user/user.actions';
 
-class App extends Component {
-  unsubscribeFromAuth = null;
-
-  componentDidMount() {
-    const { checkUserSession } = this.props;
+const App = ({ checkUserSession, currentUser }) => {
+  useEffect(() => {
     checkUserSession();
-  }
+  }, [checkUserSession]);
 
-  componentWillUnmount() {
-    this.unsubscribeFromAuth();
-  }
-
-  render() {
-    return (
-      <div>
-        <Header />
-        <Routes>
-          <Route exact path="/" element={<HomePage />} />
-          <Route exact path="/shop/*" element={<ShopPageWrapper />} />
-          <Route
-            exact
-            path={'/signin'}
-            element={
-              this.props.currentUser ? (
-                <Navigate to="/" />
-              ) : (
-                <SignInAndSignUpPage />
-              )
-            }
-          />
-          <Route exact path="/checkout" element={<CheckoutPage />} />
-        </Routes>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route exact path="/shop/*" element={<ShopPage />} />
+        <Route
+          exact
+          path={'/signin'}
+          element={currentUser ? <Navigate to="/" /> : <SignInAndSignUpPage />}
+        />
+        <Route exact path="/checkout" element={<CheckoutPage />} />
+      </Routes>
+    </div>
+  );
+};
 
 const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser,
--- src/pages/shop/shopPage.jsx
+++ src/pages/shop/shopPage.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Route, Routes, useParams } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
@@ -7,38 +7,35 @@ import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
 import CollectionsOverviewContainer from '../../componenets/collections-overview/collections-overview.container';
 import CollectionPageContainer from '../collection/collection.container';
 
-class ShopPage extends React.Component {
-  componentDidMount() {
+const ShopPage = ({ fetchCollectionsStart }) => {
+  const params = useParams();
+  useEffect(() => {
     // fetch(
     //   'https://firestore.googleapis.com/v1/projects/YOUR_PROJECT_ID/databases/(default)/documents/collections'
     // )
     //   .then((response) => response.json())
     //   .then((colections) => console.log('collections', colections));
-    const { fetchCollectionsStart } = this.props;
     fetchCollectionsStart();
-  }
+  }, [fetchCollectionsStart]);
 
-  render() {
-    const { params, isCollectionFetching } = this.props;
-    return (
-      <div>
-        <Routes>
-          <Route
-            exact
-            path={``}
-            element={
-              <CollectionsOverviewContainer isLoading={isCollectionFetching} />
-            }
-          />
-          <Route
-            path={`/:collectionId`}
-            element={<CollectionPageContainer params={params} />}
-          />
-        </Routes>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <Routes>
+        <Route
+          exact
+          path={``}
+          element={
+            <CollectionsOverviewContainer isLoading={fetchCollectionsStart} />
+          }
+        />
+        <Route
+          path={`/:collectionId`}
+          element={<CollectionPageContainer params={params} />}
+        />
+      </Routes>
+    </div>
+  );
+};
 
 const mapDispatchToProps = (dispatch) => ({
   fetchCollectionsStart: () => dispatch(fetchCollectionsStart()),
